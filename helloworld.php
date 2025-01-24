fa7ef67c7f2e12c54852a7c89a3cc91a8d76e972
UnsteadyDizzy <madison@kostersfamily.com>
2025-01-24 13:13:03 -0700

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Hello, World!</title>
+</head>
+
+<body>
+
+<h1>My first PHP page</h1>
+<p>
+<?php
+	echo "Hello World!";
+?>
+</p>
+</body>
+</html>
