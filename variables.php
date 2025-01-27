eb5701e82599f22996d98f1d3cf84768d647f16f
UnsteadyDizzy <madison@kostersfamily.com>
2025-01-27 16:14:44 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+// Information:
+$firstName = "Madison";
+$lastName = "Kosters";
+$address = "9234 Old Main Drive";
+$phoneNumber = 9284321234;
+
+// Printing the Information:
+print "<p>I am $firstName $lastName and I live at $address. You can reach me at $phoneNumber</p>";
+
+?>
+</body>
+</html>
