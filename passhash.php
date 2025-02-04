aaea8025caf39c440a31560d0aa64f7e3656c8f1
UnsteadyDizzy <madison@kostersfamily.com>
2025-02-04 15:42:24 -0700

    Add files via upload

---
+++ passhash.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Forum Posting</title>
+</head>
+<body>
+
+<?php
+
+$password = "helloclass";
+
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+echo  $hashed_password;
+
+?>
+</body>
+</html>
