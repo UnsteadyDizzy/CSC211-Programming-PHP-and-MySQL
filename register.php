a9b64b286d89cf21ec1abc25bf709e331ef61c36
UnsteadyDizzy <madison@kostersfamily.com>
2025-02-24 16:02:38 -0700

    Add files via upload

--- register.php
+++ register.php
@@ -1,81 +1,78 @@
-<?php // Script 8.9 - register.php
-/* This page lets people register for the site (in theory). */
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Register</title>
+	<style type="text/css" media="screen">
+		.error { color: red; }
+	</style>
+</head>
+<body>
+<h1>Register</h1>
+<?php // Script 11.6 - register.php
+/* This script registers a user by storing their information in a text file and creating a directory for them. */
 
-// Set the page title and include the header file:
-define('TITLE', 'Register');
-include('templates/header2.html');
+// Identify the directory and file to use:
+$dir = '../users/';
+$file = $dir . 'users.txt';
 
-// Print some introductory text:
-print '<h2>Registration Form</h2>
-	<p>Register so that you can take advantage of certain features like this, that, and the other thing.</p>';
-	
-// Check if the form has been submitted:
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form.
 
-	$problem = false; // No problems so far.
-	
+	$problem = FALSE; // No problems so far.
+
 	// Check for each value...
-	if (empty($_POST['first_name'])) {
-		$problem = true;
-		print '<p class="text--error">Please enter your first name!</p>';
-	}
-	
-	if (empty($_POST['last_name'])) {
-		$problem = true;
-		print '<p class="text--error">Please enter your last name!</p>';
-	}
-
-	if (empty($_POST['email']) || (substr_count($_POST['email'], '@') != 1) ) {
-		$problem = true;
-		print '<p class="text--error">Please enter your email address!</p>';
-	}
+	if (empty($_POST['username'])) {
+		$problem = TRUE;
+		print '<p class="error">Please enter a username!</p>';
+	}	
 
 	if (empty($_POST['password1'])) {
-		$problem = true;
-		print '<p class="text--error">Please enter a password!</p>';
+		$problem = TRUE;
+		print '<p class="error">Please enter a password!</p>';
 	}
 
 	if ($_POST['password1'] != $_POST['password2']) {
-		$problem = true;
-		print '<p class="text--error">Your password did not match your confirmed password!</p>';
+		$problem = TRUE;
+		print '<p class="error">Your password did not match your confirmed password!</p>';
 	} 
 	
 	if (!$problem) { // If there weren't any problems...
-
-		// Print a message:
-		print '<p class="text--success">You are now registered!<br>Okay, you are not really registered but...</p>';
-
-		// Send the email:
-		$body = "Thank you, {$_POST['first_name']}, for registering with the J.D. Salinger fan club!'.";
-		mail($_POST['email'], 'Registration Confirmation', $body, 'From: admin@example.com');
-		ini_set("stmp_port", 80);
-
-		// Clear the posted values:
-		$_POST = [];
 	
-	} else { // Forgot a field.
-	
-		print '<p class="text--error">Please try again!</p>';
+		if (is_writable($file)) { // Open the file.
+			
+			// Create the data to be written:
+			$subdir = time() . rand(0, 4596);
+			$data = $_POST['username'] . "\t" . sha1(trim($_POST['password1'])) . "\t" . $subdir . PHP_EOL;
+
+			// Write the data:
+			file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+			
+			// Create the directory:
+			mkdir ($dir . $subdir);
+
+			// Print a message:
+			print '<p>You are now registered!</p>';
 		
+		} else { // Couldn't write to the file.
+			print '<p class="error">You could not be registered due to a system error.</p>';
+		}
+		
+	} else { // Forgot a field.
+		print '<p class="error">Please go back and try again!</p>';	
 	}
 
-} // End of handle form IF.
+} else { // Display the form.
 
-// Create the form:
+// Leave PHP and display the form:
 ?>
-<form action="register.php" method="post" class="form--inline">
 
-	<p><label for="first_name">First Name:</label><input type="text" name="first_name" size="20" value="<?php if (isset($_POST['first_name'])) { print htmlspecialchars($_POST['first_name']); } ?>"></p>
-
-	<p><label for="last_name">Last Name:</label><input type="text" name="last_name" size="20" value="<?php if (isset($_POST['last_name'])) { print htmlspecialchars($_POST['last_name']); } ?>"></p>
-
-	<p><label for="email">Email Address:</label><input type="email" name="email" size="20" value="<?php if (isset($_POST['email'])) { print htmlspecialchars($_POST['email']); } ?>"></p>
-
-	<p><label for="password1">Password:</label><input type="password" name="password1" size="20" value="<?php if (isset($_POST['password1'])) { print htmlspecialchars($_POST['password1']); } ?>"></p>
-	<p><label for="password2">Confirm Password:</label><input type="password" name="password2" size="20" value="<?php if (isset($_POST['password2'])) { print htmlspecialchars($_POST['password2']); } ?>"></p>
-
-	<p><input type="submit" name="submit" value="Register!" class="button--pill"></p>
-
+<form action="register.php" method="post">
+	<p>Username: <input type="text" name="username" size="20"></p>
+	<p>Password: <input type="password" name="password1" size="20"></p>
+	<p>Confirm Password: <input type="password" name="password2" size="20"></p>
+	<input type="submit" name="submit" value="Register">
 </form>
 
-<?php include('templates/footer2.html'); // Need the footer. ?>
+<?php } // End of submission IF. ?>
+</body>
+</html>
